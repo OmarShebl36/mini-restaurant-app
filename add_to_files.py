15308a0863317ecda3876d8a310507cd003e6a16
Omar Shebl <Omarshebl36@outlook.com>
2023-03-09 16:44:42 +0200

    Updated the add to file program

--- add_to_files.py
+++ add_to_files.py
@@ -1,5 +1,7 @@
 import restaurant_model
 import menu_item_model
+from constants import restaurants
+import csv
 
 """
 This file is made for a development purpose.
@@ -7,26 +9,73 @@ It can be used to add new restaurants and menu items.
 """
 
 # Setup
-restaurant_name = input("Restaurant name: ")
-restaurant_img_name = input("Restaurant img name: ")
-menu_itme_name = input("Menu itme name: ")
-menu_itme_price = float(input("Menu itme price: "))
+restaurant_name = ''
+restaurant_img_name = ''
+menu_item_name = ''
+menu_item_price = 0
+stay = True
 
-# Create a restaurant instance
-restaurant_model.add_new_restaurant(
-    name=restaurant_name,
-    img_src=f"static/images/{restaurant_img_name}",
-    )
-
-# 
-mcdonald = restaurant_model.Restaurant(restaurant_name, restaurant_img_name)
-
-# Get restaurant's menu items
-mcdonald.get_menu_items()
-
-# Create a menu_item
-item = menu_item_model.add_menu_item(
-    restaurant=mcdonald,
-    name=menu_itme_name,
-    price=menu_itme_price,
-    )
+# Implementation
+while True:
+    answer = input("What do you want to do? \n(r for adding a new restaurant, m for adding a new menu item, q to quit):\n")
+    match answer:
+        case 'r': 
+            restaurant_name = input("Restaurant name: ")
+            restaurant_img_name = input("Restaurant img name: ")
+            restaurant_img_name = f"static/images/{restaurant_img_name}"
+            try:
+                open(restaurant_img_name)           
+                # Create a restaurant
+                restaurant = restaurant_model.Restaurant(restaurant_name, restaurant_img_name)
+                # Get restaurant's menu items
+                restaurant.get_menu_items()
+                # Create a restaurant instance
+                restaurant_model.add_new_restaurant(
+                    name=restaurant_name,
+                    img_src=restaurant_img_name,
+                    )
+            except:
+                print("The image is not available. Please try another.")
+                continue
+        case 'm':
+            menu_item_name = input("Menu item name: ")
+            try:
+                menu_item_price = float(input("Menu item price: "))
+                print("Which restaurant do you want to add the menu item to? ")
+                with open('static/csv_files/restaurants.csv', newline='') as f:
+                    reader = csv.DictReader(f)
+                    for line in reader:
+                        print(f"Choose {line['id']} for adding to {line['name']}")
+                choice = input("")
+                for res in restaurants:
+                    if res.id == choice:
+                        restaurant = res
+                # Create a menu_item
+                menu_item_model.add_menu_item(
+                    restaurant=restaurant,
+                    name=menu_item_name,
+                    price=menu_item_price,
+                )
+            except:
+                print("Invalid value! Please use numbers for the menu item prices.")
+                continue
+        case 'q':
+            break
+        case _:
+            print("Invalid value! Please try again.")
+            boolean = True
+            while boolean:
+                answer = input("Try again? (y/n): ")
+                if answer == "n":
+                    boolean = False
+                    break
+                elif answer != "y":
+                    print("Invalid answer! Please try again.")
+                    continue
+                else:
+                    boolean = True
+                    break
+            if boolean:
+                continue
+            else:
+                break
--- menu_item_model.py
+++ menu_item_model.py
@@ -3,7 +3,7 @@ import restaurant_model
     
 class Menu_item:
     # selected_counter is used to determine the number of times the item is selected
-    def __init__(self, name, price, restaurant_name, id):
+    def __init__(self, name, price, restaurant_name, id=0):
         self.name = name
         self.id = id
         self.price = price
@@ -13,12 +13,14 @@ class Menu_item:
 # Add new items to menu_items file
 def add_to_file(name, price, restaurant_name, id,):
     field_names = ["name", "price", "restaurant_name", "id"]
+    # Checks if the item is already in the file or not
     with open('static/csv_files/menu_items.csv', newline='') as f:
         csv_reader = csv.DictReader(f, fieldnames=field_names)
         for row in csv_reader:
             if row['name'] == name and row['restaurant_name'] == restaurant_name:
                 return
         
+    # If not add it to the file
     with open('static/csv_files/menu_items.csv', 'a', newline='') as f:
         csv_writer = csv.DictWriter(f, fieldnames=field_names)
         csv_writer.writerow(
@@ -31,6 +33,18 @@ def add_to_file(name, price, restaurant_name, id,):
 
 # Add new menu_item
 def add_menu_item(name, price, restaurant):
-    id = len(restaurant.menu)
+    counter = 0
+    # Counts the number of items to give a different id
+    try:
+        with open("static/csv_files/menu_items.csv", newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row['name']:
+                    counter += 1
+    except e:
+        counter = 0
+        print(e)
+        
+    id = counter
     new_menu_item = Menu_item(name, price, restaurant.name, id)
     add_to_file(name=name, price=price, restaurant_name=restaurant.name, id=id)
